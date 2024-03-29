afe147f076c70079a758a6c3b412687ea05e2388
Matt Bramlage <mbramlage@gmail.com>
2024-03-29 12:29:54 -0700

    Added some nice split outs for the library; configurable creds

--- slack_me/message.py
+++ slack_me/message.py
@@ -16,43 +16,57 @@ def resource(name):
     else:
         return pathlib.Path(__file__).parent / name
 
+
 credentials = None
-credentials_file = resource("credentials.yaml")
-with open(str(credentials_file)) as f:
-    credentials = yaml.safe_load(f)
+def obtain_credentials(credentials_file = resource("credentials.yaml")):
+    with open(str(credentials_file)) as f:
+        credentials = yaml.safe_load(f)
+    return credentials
 
 # WebClient instantiates a client that can call API methods
 # When using Bolt, you can use either `app.client` or the `client` passed to listeners.
-client = WebClient(token=credentials['bot_token'])
 LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.DEBUG)
 
-def get_bots_channel():
-    conversation_id = None
-    try:
-        # Call the conversations.list method using the WebClient
-        for result in client.conversations_list():
-            for channel in result["channels"]:
-                if channel['name'] == "bots":
-                    conversation_id = channel["id"]
-                    LOG.info(f"Found channel id {conversation_id} for channel \"bots\"")
-                print(f"Found conversation: {channel['name']} : {conversation_id}")
-    except SlackApiError as e:
-        print(f"Error: {e}")
-    return conversation_id
+class SlackBotsChannelMessenger:
 
-def post_message(message, channel_id = get_bots_channel()):
-    try:
-        # Call the conversations.list method using the WebClient
-        result = client.chat_postMessage(
-                channel=channel_id,
-                text=message
-                )
-        LOG.info(f"Message post result: {result}")
+    def __init__(self, credentials):
+        self._client = WebClient(token=credentials['bot_token'])
+        self._bots_channel_id = None
 
-    except SlackApiError as e:
-            print(f"Error: {e}")
+    @property
+    def bots_channel_id(self):
+        if not self._bots_channel_id:
+            LOG.info("Getting bots channel...")
+            try:
+                # Call the conversations.list method using the WebClient
+                for result in self._client.conversations_list():
+                    for channel in result["channels"]:
+                        if channel['name'] == "bots":
+                            # We found the bots channel
+                            self._bots_channel_id = channel["id"]
+                            LOG.info(f"Found channel id {self._bots_channel_id} for channel \"bots\"")
+            except SlackApiError as e:
+                ASSERT(False, e)
+        return self._bots_channel_id
+
+    def post_message(self, message, channel_id = None):
+        if not channel_id: channel_id = self.bots_channel_id
+        try:
+            # Call the conversations.list method using the WebClient
+            result = self._client.chat_postMessage(
+                    channel=channel_id,
+                    text=message
+                    )
+            LOG.info(f"Message post result: {result}")
+
+        except SlackApiError as e:
+                print(f"Error: {e}")
 
 if __name__ == "__main__":
+    # When run not as a library, do our base logging config.
+    import python_logging_base
+
     # Notice the handling of default arguments from a file...
     default_arguments_file = resource("defaults.yaml")
     with open(str(default_arguments_file)) as f:
@@ -65,4 +79,10 @@ if __name__ == "__main__":
     # distributed app bundles to be modified at launch / changed default file values.
     parsed_args = parser.parse_args()
     args.update((k, v) for k,v in vars(parsed_args).items() if v is not None)
-    post_message(args['message'])
+
+    # Set up the credentials
+    c = obtain_credentials()
+    messenger = SlackBotsChannelMessenger(c)
+    message = args['message']
+    LOG.info(f'Posting message "{message}"')
+    messenger.post_message(args['message'])
